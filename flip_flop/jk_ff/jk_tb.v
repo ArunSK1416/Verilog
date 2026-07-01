91341c8fa5b17556fc4628c16bccdee45db67b02
ARUN K <skarunskarun293@gmail.com>
2026-07-01 10:07:09 +0530

    Update and rename flip_flop/jk_tb.v to flip_flop/jk_ff/jk_tb.v

--- flip_flop/jk_ff/jk_tb.v
+++ flip_flop/jk_ff/jk_tb.v
@@ -10,9 +10,12 @@ initial begin
 $monitor("time = %0t || clk = %b || j = %b || k = %b || q = %b ",$time,clk,j,k,q);
 clk = 0;
 j=0;k=0;
-#10 j= 0; k=1;
-#10; j=1; k=0;
-#10 j = 1; k=1;
+#15 j = 0; k = 0;   // Hold
+#20 j = 0; k = 1;   // Reset
+#20 j = 1; k = 0;   // Set
+#20 j = 1; k = 1;   // Toggle
+#20 j = 0; k = 0;   // Hold
+
 #20 $finish;
 
 end
