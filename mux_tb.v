eee06cb3158d2fadfd38fcedb8183630831424e9
ARUN K <skarunskarun293@gmail.com>
2026-07-02 09:47:12 +0530

    Add files via upload

---
+++ mux_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module tb_mux_21;
+
+    reg a;
+    reg b;
+    reg s;
+    wire z;
+    mux_21 uut (
+        .a(a),
+        .b(b),
+        .s(s),
+        .z(z)
+    );
+    initial begin
+        $monitor("Time=%0t | s=%b a=%b b=%b -> z=%b",
+                  $time, s, a, b, z);
+        a = 0; b = 0; s = 0; #10;
+        a = 1; b = 0; s = 0; #10;
+        a = 0; b = 1; s = 0; #10;
+        a = 1; b = 1; s = 0; #10;
+
+        a = 0; b = 0; s = 1; #10;
+        a = 1; b = 0; s = 1; #10;
+        a = 0; b = 1; s = 1; #10;
+        a = 1; b = 1; s = 1; #10;
+
+        $finish;
+    end
+
+endmodule
