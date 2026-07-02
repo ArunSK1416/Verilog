3819c79b033984382571354862fb7527bd9fd5de
ARUN K <skarunskarun293@gmail.com>
2026-07-02 10:18:07 +0530

    Update mux_tb.v

--- combinations circuit/mux_16/mux_tb.v
+++ combinations circuit/mux_16/mux_tb.v
@@ -1,19 +1,13 @@
 `timescale 1ns / 1ps
 
-module multiplex12_tb;
+module mux_tb;
 
 reg k0,k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,k14,k15;
 reg s0,s1,s2,s3;
 wire x;
 
-multiplex12 uut (
-    .k0(k0), .k1(k1), .k2(k2), .k3(k3),
-    .k4(k4), .k5(k5), .k6(k6), .k7(k7),
-    .k8(k8), .k9(k9), .k10(k10), .k11(k11),
-    .k12(k12), .k13(k13), .k14(k14), .k15(k15),
-    .x(x),
-    .s0(s0), .s1(s1), .s2(s2), .s3(s3)
-);
+multiplex12 uut (.k0(k0), .k1(k1), .k2(k2), .k3(k3),.k4(k4), .k5(k5), .k6(k6), .k7(k7),.k8(k8), .k9(k9), .k10(k10), .k11(k11),
+                 .k12(k12), .k13(k13), .k14(k14), .k15(k15),.x(x),.s0(s0), .s1(s1), .s2(s2), .s3(s3));
 
 initial begin
     {k15,k14,k13,k12,k11,k10,k9,k8,k7,k6,k5,k4,k3,k2,k1,k0} = 16'b1010101111001101;
